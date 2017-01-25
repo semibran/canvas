da0d4cba017f6a0c065ff87618d03fdf7555ad86
Brandon Semilla <semibran@gmail.com>
2017-01-24 16:18:38 -0800

    Add new lib and docs

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # canvas
-Wrapper for HTML Canvas API with a focus on usability
+> ~~usable~~ unstable wrapper for canvas api
+
+```javascript
+var canvas = Canvas(width, height)
+  .rect(fill)(x, y, width, height)
+  .circle(fill)(x, y, radius)
+  .image(image)(x, y, width, height)
+
+document.body.appendChild(canvas.element)
+```
+
+## Documentation
+WIP
+
+### Factory
+```javascript
+// Create from an existing `CanvasRenderingContext2D`...
+var canvas = Canvas(context)
+
+// ...or from dimensions to create a blank slate.
+var canvas = Canvas(width, height)
+```
+Creates and returns a new `Canvas` object from the specified `CanvasRenderingContext2D`. Alternatively, you can create a new `Canvas` instance from scratch by passing in the desired `width` and `height`.
+
+### Methods
+The primary drawing methods (`rect`, `circle`, `image`) have been curried, allowing you to draw multiple shapes of the same fill by storing the result in a variable for later use (below). They also return the `Canvas` instance, which enables method chaining (above).
+
+```javascript
+// Making use of currying to draw some blue rectangles...
+var drawBlueRect = canvas.rect('blue')
+drawBlueRect(...rect1)
+drawBlueRect(...rect2)
+```
+
+#### `rect`
+```javascript
+canvas.rect(fill)(x, y, width, height) // -> canvas
+```
+Draws a rectangle of the given color and dimensions onto the canvas context.
+
+#### `circle`
+```javascript
+canvas.circle(fill)(x, y, radius) // -> canvas
+```
+Draws a circle of the given color and dimensions onto the canvas context.
+
+#### `image`
+```javascript
+canvas.circle(image)(x, y, width, height) // -> canvas
+```
+Draws the provided image onto the canvas ontext using the given dimensions.
+
+#### `getPixels`
+```javascript
+var pixels = canvas.getPixels() // -> pixels
+```
+Returns a [`Pixels`](https://github.com/semibran/pixels) instance based on the canvas pixel data.
+
+#### `setPixels`
+```javascript
+canvas.setPixels(pixels) // -> canvas
+```
+Applies the given `Pixels` instance onto the canvas context.
+
+### Properties
+
+#### `width`
+The width of the `Canvas` instance.
+
+#### `height`
+The height of the `Canvas` instance.
+
+#### `element`
+The `HTMLCanvasElement` which is used to view the results of method use. Equivalent to `canvas.context.canvas`.
+
+```javascript
+document.body.appendChild(canvas.element)
+```
+
+#### `context`
+The `CanvasRenderingContext2D` on which all operations are performed.
+
+## License
+MIT
--- test/index.js
+++ test/index.js
@@ -1,87 +1,11 @@
-const load = require('../utils/image')
-const Canvas = require('../utils/canvas')
-const Sprite = require('../utils/sprite')
-const Display = require('../utils/display')
-const Menu = require('../utils/menu')
+var Canvas = require('../lib')
 
-let wrap = document.querySelector('#app')
+var canvas = Canvas(256, 224)
+  .rect('black')()
+  .circle('white')(128, 112, 32)
 
-let display = Display(256, 224)
-wrap.appendChild(display.canvas.element)
+var pixels = canvas.getPixels()
+pixels.replace('white', 'crimson')
+canvas.setPixels(pixels)
 
-let menu, cursor
-
-let dir = 'images/'
-let ext = '.png'
-
-let names = ['background', 'select', 'menu', 'mario', 'text', 'tiles']
-let paths = names.map(name => dir + name + ext)
-
-load(...paths)(setup)
-
-function setup(images) {
-
-  let squareSize = 8
-  let sprites = Sprite(images.menu)(squareSize)()
-
-  let background = Display.Node(images.background)
-
-  let cols = display.width / (squareSize * Menu.squares)
-  let rows = 2
-  menu = Menu(sprites)(cols, rows)
-  menu.y = display.height - menu.height
-
-  let mario = Display.Node(Sprite(images.mario)(16)(0))
-  mario.x = menu.width  / 2
-  mario.y = menu.height / 2
-  mario.origin = 'center'
-
-  cursor = Display.Node(Sprite(images.select)(16)(4))
-
-  menu.on
-    ('animate', () => display.render(menu))
-    ('show',    () => display.render(menu.add(mario).render()))
-    ('hiding',  () => display.render(menu.remove(mario).render()))
-
-  display.add(background, menu, cursor).render()
-
-  window.addEventListener('keydown', handleKeys)
-  window.addEventListener('keyup',   handleKeys)
-  wrap.addEventListener('mousemove', handleMouse)
-
-  loop()
-
-}
-
-function loop() {
-
-  if (!menu.animation)
-    if (keys.Space) {
-      if (!menu.visible)
-        menu.show()
-    } else if (menu.visible)
-      menu.hide()
-
-  requestAnimationFrame(loop)
-
-}
-
-let keys = {}
-function handleKeys(event) {
-  keys[event.code] = event.type === 'keydown'
-}
-
-let tileSize = 16
-let displayCols = display.width  / tileSize
-let displayRows = display.height / tileSize
-function handleMouse(event) {
-  let { offsetX: x, offsetY: y } = event
-  let rect = wrap.getBoundingClientRect()
-  x = Math.floor(x / rect.width  * displayCols)
-  y = Math.floor(y / rect.height * displayRows)
-  if (cursor.x / tileSize - x || cursor.y / tileSize - y) {
-    cursor.x = x * tileSize
-    cursor.y = y * tileSize
-    display.render()
-  }
-}
+document.body.appendChild(canvas.element)
